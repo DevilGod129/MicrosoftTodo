2ee900ac4c9cb41a8d98ffc97a9124a9df768811
Prasun Paudel <paudelprasun@gmail.com>
2025-02-28 23:25:53 +0545

    Added the features

--- src/features/todoSlice.ts
+++ src/features/todoSlice.ts
@@ -1,7 +1,22 @@
-import { createSlice,nanoid } from "@reduxjs/toolkit"; 
-
-
-const initialState = {
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+interface TodoSlice {
+    todos : NewTodos[],
+}
+interface NewTodos {
+    todo_id: string;
+    text: string;
+    completed: boolean;
+    subtodo: NewSubTodo[],
+    list_id:string,
+    timestamp: Date,
+    important: boolean, 
+}
+interface NewSubTodo {
+    subtodo_id: string,
+    subtodo_text: string,
+    subtodo_completed: boolean,
+}
+const initialState:TodoSlice ={
     todos:[],    
 }
 
@@ -12,7 +27,7 @@ export const TodoSlice = createSlice({
         // Addding todo to the string
         addTodo: (state,action) => {
             const {list_id } = action.payload
-            const add_new_todo = {
+            const add_new_todo: NewTodos = {
                 todo_id: nanoid(),
                 text: action.payload(),
                 completed: false,
@@ -26,15 +41,15 @@ export const TodoSlice = createSlice({
 
         // Deleting todo 
         deleteTodo: (state,action) => {
-            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
+            state.todos = state.todos.filter((todo) => todo.todo_id !== action.payload )
             // Here the filter returns back a new array with removed id todo
         },
 
         // Edit the text in todo.......
         editTodo: (state,action) => {
             const {id,text} =action.payload
-            state.todos = state.todos.map((todo) => todo.id === id ? {...todo,text:text}:todo)
-            state.todos = state.todos.map((todo) => todo.id === id ? {...todo,timestamp:new Date()}:todo)
+            state.todos = state.todos.map((todo) => todo.todo_id === id ? {...todo,text:text}:todo)
+            state.todos = state.todos.map((todo) => todo.todo_id === id ? {...todo,timestamp:new Date()}:todo)
 
 
         },
@@ -43,39 +58,109 @@ export const TodoSlice = createSlice({
 
         toggleTodo: (state,action) =>{
             const {id} =action.payload
-            state.todos = state.todos.map((todo) => todo.id === id ? {...todo,completed: !todo.completed}:todo)
+            state.todos = state.todos.map((todo) => todo.todo_id === id ? {...todo,completed: !todo.completed}:todo)
         },
 
         //set the todo as important....
 
-        importantTodo: (state,action) => {
-            const {id}  = action.payload
-            state.todos = state.todos.map((obj) => obj.id=== id ? {...obj,important:!state.important}: obj)
+        importantTodo: (state,action: {
+            payload : {
+                todo_id :string
+            }
+        }) => {
+            const {todo_id}  = action.payload
+            state.todos = state.todos.map((obj) => obj.todo_id=== todo_id ? {...obj,important:!state.todos}: obj)
         },
 
         // substeps: 
 
-        add_sub_todo: (state,action) =>{
-            const subtodo_add  = {
+        add_sub_todo: (state,action : {
+            payload : {
+                todoId:string,
+                textContent:string
+
+
+            }
+        }) =>{
+
+            const todoId = action.payload.todoId;
+            const subtodo_add:NewSubTodo  = {
                 subtodo_id: nanoid(),
-                subtodo_text: action.payload(),
+                subtodo_text: action.payload.textContent,
                 subtodo_completed: false,
             }
-            state.todos.subtodo.push(subtodo_add)
+            state.todos.find((v)=> v.todo_id == todoId)?.subtodo.push(subtodo_add)
         },
 
-        del_sub_todo : (state,action) => {
-            state.todos.subtodo = state.todos.subtodo.filter((todo) => todo.subtodo_id !== action.payload )
+        del_sub_todo : (state,action:{
+            payload : {
+                todoId: string,
+                subtodoId: string,
+            }
+        }) => {
+
+            // state.todos.subtodo = state.todos.subtodo.filter((todo) => todo.subtodo_id !== action.payload )
+            const TodoId = action.payload.todoId
+            const subTodoId = action.payload.subtodoId
+
+            state.todos.find((v) => v.todo_id == TodoId)?.subtodo.filter((todo) => todo.subtodo_id !== subTodoId)
+
         },
 
-        edit_sub_todo : (state,action) => {
-            const {id,text} =action.payload
-            state.todos.subtodo = state.todos.subtodo.map((todo) => todo.subtodo_id === id ? {...todo,subtodo_text:text}:todo)
+        edit_sub_todo : (state,action:{
+            payload:{
+                TodoId : string,
+                subTodoId : string,
+                subTodoContent: string,
+
+            }
+        }) => {
+            
+            // state.todos.subtodo = state.todos.subtodo.map((todo) => todo.subtodo_id === id ? {...todo,subtodo_text:text}:todo)
+            const TodoId = action.payload.TodoId
+            const subTodoId = action.payload.subTodoId
+            const subTodoContent = action.payload.subTodoContent
+
+            // state.todos.find((v) => v.todo_id == TodoId)?.subtodo.find((todo) => todo.subtodo_id == subTodoId)?.subtodo_text = subTodoContent
+            const todo = state.todos.find((v) => v.todo_id == TodoId)
+            if(todo){
+                const subtodo = todo.subtodo.find((todo) => todo.subtodo_id == subTodoId)
+                if(subtodo){
+                    subtodo.subtodo_text = subTodoContent
+                }
+                else{
+                    return 
+                }
+            }
+            else{
+                return
+            }
         },
         
-        toggle_sub_todo: (state,action) =>{
-            const {id} =action.payload
-            state.todos.subtodo = state.todos.subtodo.map((todo) => todo.subtodo_id === id ? {...todo,subtodo_completed: !todo.subtodo_completed}:todo)
+        toggle_sub_todo: (state,action:{
+            payload:{
+                TodoId : string,
+                subTodoId : string,
+            }
+        }) =>{
+            const TodoId = action.payload.TodoId
+            const subTodoId = action.payload.subTodoId
+
+            // state.todos.subtodo = state.todos.subtodo.map((todo) => todo.subtodo_id === id ? {...todo,subtodo_completed: !todo.subtodo_completed}:todo)
+            const todo = state.todos.find((v) => v.todo_id == TodoId)
+            if(todo){
+                const subtodo = todo.subtodo.find((todo) => todo.subtodo_id == subTodoId)
+                if(subtodo){
+                    subtodo.subtodo_completed = !subtodo.subtodo_completed
+                }
+                else{
+                    return 
+                }
+            }
+            else{
+                return
+            }
+
         },
 
     }
